d4da6517c9e636bd17b399264362f91c535c31c8
Beiwen Tian <tianbeiwen88@gmail.com>
2024-06-05 03:18:28 +0800

    update dual arms and gripper

--- airbot_receiver.py
+++ airbot_receiver.py
@@ -5,25 +5,30 @@ import functools
 import asyncio
 import websockets
 
-async def handler(robot, args, websocket):
+async def handler(robots, args, websocket):
+    robot_left, robot_right = robots
     await websocket.send("Welcome to the WebSocket server!")
     async for message in websocket:
         try:
-            joints = pickle.loads(message)
-            print(joints)
-            robot.set_target_joint_mit(joints, [0,0,0,0,0,0], [100,100,100,10,10,10], [1,1,1,0.2,0.2,0.2])
+            info = pickle.loads(message)
+            robot_left.set_target_joint_mit(info['left'], [0,0,0,0,0,0], [100,100,100,10,10,10], [1,1,1,0.2,0.2,0.2])
+            robot_left.set_target_end(info['left_end'])
+            robot_right.set_target_joint_mit(info['right'], [0,0,0,0,0,0], [100,100,100,10,10,10], [1,1,1,0.2,0.2,0.2])
+            robot_right.set_target_end(info['right_end'])
         except Exception as e:
             print(e)
 
 async def main(args):
-    robot = airbot.create_agent(can_interface=args.master, end_mode='gripper')
-    f = functools.partial(handler, robot, args)
+    robot_left = airbot.create_agent(can_interface=args.left, end_mode='gripper')
+    robot_right = airbot.create_agent(can_interface=args.right, end_mode='gripper')
+    f = functools.partial(handler, (robot_left, robot_right), args)
     async with websockets.serve(f, args.host, int(args.port)):
         await asyncio.Future()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Send data to the airbot')
-    parser.add_argument("-m", "--master", help="Sender CAN Interface", default='can0')
+    parser.add_argument("-l", "--left", help="Sender CAN Interface", default='can0')
+    parser.add_argument("-r", "--right", help="Sender CAN Interface", default='can1')
     parser.add_argument("-H", "--host", default='0.0.0.0')
     parser.add_argument("-p", "--port", help="Serial port", default='18188')
     parser.add_argument("-b", "--baudrate", help="Serial baudrate", default=9600)
--- airbot_sender.py
+++ airbot_sender.py
@@ -6,17 +6,23 @@ import time
 import pickle
 
 async def main(args):
-    robot = airbot.create_agent(can_interface=args.master)
+    robot_left = airbot.create_agent(can_interface=args.left)
+    robot_right = airbot.create_agent(can_interface=args.right)
     uri = f"ws://{args.host}:{args.port}"
     async with websockets.connect(uri) as websocket:
         while True:
-            print(robot.get_current_joint_q())
             time.sleep(0.0005)
-            await websocket.send(pickle.dumps(robot.get_current_joint_q()))
+            await websocket.send(pickle.dumps({
+                'left': robot_left.get_current_joint_q(),
+                'right': robot_right.get_current_joint_q(),
+                'left_end': robot_left.get_current_end(),
+                'right_end': robot_right.get_current_end(),
+            }))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Send data to the airbot')
-    parser.add_argument("-m", "--master", help="Sender CAN Interface", default='can0')
+    parser.add_argument("-l", "--left", help="Sender CAN Interface", default='can0')
+    parser.add_argument("-r", "--right", help="Sender CAN Interface", default='can1')
     parser.add_argument("-H", "--host", default='0.0.0.0')
     parser.add_argument("-p", "--port", help="Serial port", default='18188')
     parser.add_argument("-b", "--baudrate", help="Serial baudrate", default=9600)
